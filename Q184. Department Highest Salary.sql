52ae1417322bc0c7624a41df3d5aede08cbf580d
tuiiiya <69383612+smyry1@users.noreply.github.com>
2022-03-16 14:32:27 +0800

    Create Q184. Department Highest Salary.sql

---
+++ Q184. Department Highest Salary.sql
@@ -0,0 +1,51 @@
+--Problem
+/*
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key column for this table.
+departmentId is a foreign key of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+Write an SQL query to find employees who have the highest salary in each of the departments.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/department-highest-salary
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+
+-Solution
+SELECT
+d.name AS Department,
+e.name AS Employee,
+e.salary AS salary
+FROM Employee e, Department d WHERE e.departmentId = d.id
+AND (salary,e.departmentId) IN
+(SELECT MAX(salary),departmentId FROM Employee
+GROUP BY departmentId)
